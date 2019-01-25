76fae69d7288c314f2cefffffc4ec3d0675142a3
andreasnn <andreas@fides.dk>
2019-01-25 19:33:07 +0100

    Chroot environment added etc.

--- create-package.sh
+++ create-package.sh
@@ -1,20 +1,62 @@
 version=2019-1
 
-# Get the source code
-wget https://github.com/patrikolausson/dispmanx_vnc/archive/master.zip
-unzip master.zip
 
-# Compile
-make -C dispmanx_vnc-master
+function chroot()
+{
+	chroot=~/Downloads/filesystem
 
-# Copy bin file to bin folder
-mv dispmanx_vnc-master/dispmanx_vncserver dispmanx-vncserver/usr/bin
+	mkdir $chroot/share
+	sudo mount -o bind . $chroot/share
+	sudo mount -o bind /dev $chroot/dev
 
-# Remove files
-rm -fr master.zip dispmanx_vnc-master
+	# Network setup
+	sudo cp /etc/resolv.conf $chroot/etc
 
-# Create the package
-dpkg-deb --build dispmanx-vncserver dispmanx-vncserver_$version.deb
+	sudo chroot $chroot /bin/bash /share/create-package.sh
 
-# Add this to post installation
-#systemctl enable dispmanx_vncserver
+	sudo umount $chroot/dev
+	sudo umount $chroot/share
+}
+
+function install_packages()
+{
+	# Install needed packages
+	apt-get update
+	apt-get -y dist-upgrade
+	apt-get install -y build-essential rbp-userland-dev-osmc libvncserver-dev libconfig++-dev
+}
+
+function compile_dispmanx()
+{
+	cd /share
+
+	# Get the source code
+	git clone https://github.com/patrikolausson/dispmanx_vnc.git
+
+	# Compile
+	make -C dispmanx_vnc
+
+	# Copy bin file to bin folder
+	mv dispmanx_vnc/dispmanx_vncserver dispmanx-vncserver/usr/bin
+}
+
+function build_package()
+{
+	# Create the package
+	dpkg-deb --build dispmanx-vncserver dispmanx-vncserver_$version.deb
+
+	# Add this to post installation
+	#systemctl enable dispmanx_vncserver
+}
+
+
+ischroot
+
+if [ $? == 1 ]
+then
+	chroot
+else
+	install_packages
+	compile_dispmanx
+	build_package
+fi
